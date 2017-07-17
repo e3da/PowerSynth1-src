eca8d05c9917ea52ccf3af3a4326e7c25a74fe12
John Alumbaugh <jcalumba@email.uark.edu>
2017-07-17 14:20:49 -0500

    refactored code to be split into unitary class files

---
+++ powercad/corner_stitch/CSCG.py
@@ -0,0 +1,189 @@
+import numpy as np
+from matplotlib.patches import Circle, Wedge
+from matplotlib.collections import PatchCollection
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import pylab
+
+class CSCG:
+    """
+    a cornerstitch representation with edges corresponding to constraint graph edges bolded
+    """
+    def __init__(self, CS, CG):
+        self.CS = CS
+        self.CG = CG
+
+    #def drawCGLines(self, figure):
+
+    def drawZeroDimsVertices(self):
+        """
+        derive and return a list of wedges which will represent vertices on the CS drawing
+        """
+        wedgeList = []
+        if self.CS.orientation == 'v':
+            for point in self.CG.zeroDimensionList:
+                wedgeList.append(Wedge((0, point), .5, 0, 360, width = 1))
+        elif self.CS.orientation == 'h':
+            for point in self.CG.zeroDimensionList:
+                wedgeList.append(Wedge((point, 0), .5, 0, 360, width = 1))
+
+        print "wedgeList length = ", len(wedgeList)
+        return wedgeList
+
+    #figure this out later
+    def findGraphEdges(self):
+        matrixCopy = np.copy(self.CG.vertexMatrix)
+        print matrixCopy[0][1]
+        edgeList = []#find the unique X/Y coordinate connections
+        arrowList = []#contains the information for arrows to be drawn in matplotlib (x,y,dx,dy)
+
+        it = np.nditer(self.CG.vertexMatrix, flags=['multi_index'])
+        while not it.finished:
+            if it[0] != 0:
+                edgeList.append((it.multi_index))
+            it.iternext()
+
+        if self.CS.orientation == 'h':
+            for stitch in self.CS.stitchList:
+                for edge in edgeList:
+                    if (self.CG.zeroDimensionList[edge[0]] == stitch.cell.getX()
+                            and self.CG.zeroDimensionList[edge[1]] == stitch.cell.getX() + stitch.getWidth()):
+                        arrowList.append((stitch.cell.getX(), stitch.cell.getY(), stitch.getWidth(), 0))
+                        edgeList.remove(edge)
+        elif self.CS.orientation == 'v':
+            for stitch in self.CS.stitchList:
+                for edge in edgeList:
+                    if (self.CG.zeroDimensionList[edge[0]] == stitch.cell.getY()
+                             and self.CG.zeroDimensionList[edge[1]] == stitch.cell.getY() + stitch.getHeight()):
+                        arrowList.append((stitch.cell.getX(), stitch.cell.getY(), 0, stitch.getHeight()))
+                        edgeList.remove(edge)
+
+        for foo in arrowList:
+            print foo
+        return arrowList
+
+    def setAxisLabels(self, plt):
+        if self.CS.orientation == 'v':
+            labels = ('Y' + str(i) for i in range(0, len(self.CG.zeroDimensionList)))
+            plt.yticks(self.CG.zeroDimensionList, list(labels))
+        elif self.CS.orientation == 'h':
+            labels = ('X' + str(i) for i in range(0, len(self.CG.zeroDimensionList)))
+            plt.xticks(self.CG.zeroDimensionList, list(labels))
+
+    def drawLayer(self, truePointer = False):
+        """
+        Draw all cells in this cornerStitch with stitches pointing to their stitch neighbors
+        TODO:
+         Also should probably change the dimensions of the object window depending on the cornerStitch size.
+        """
+        fig1 = matplotlib.pyplot.figure()
+
+        for cell in self.CS.stitchList:
+
+            ax1 = fig1.add_subplot(111, aspect='equal')
+            if not cell.cell.type == "EMPTY":
+                pattern = '\\'
+            else:
+                pattern = ''
+
+            ax1.add_patch(
+                matplotlib.patches.Rectangle(
+                    (cell.cell.x, cell.cell.y),  # (x,y)
+                    cell.getWidth(),  # width
+                    cell.getHeight(),  # height
+                    hatch = pattern,
+                    fill=False
+                )
+            )
+            #NORTH pointer
+            if cell.getNorth() == self.CS.northBoundary:
+                dx = 0
+                dy = 0
+            elif truePointer:
+                dx = cell.getNorth().cell.x + (.5 * cell.getNorth().getWidth()) - (cell.cell.x + cell.getWidth()) + .5
+                dy = cell.getNorth().cell.y + (.5 * cell.getNorth().getHeight()) - (cell.cell.y + cell.getHeight()) + .5
+            else:
+                dx =  0
+                dy = .75
+
+            ax1.arrow((cell.cell.x + cell.getWidth() - .5),
+                      (cell.cell.y + cell.getHeight()- .5),
+                      dx,
+                      dy,
+                      head_width = .25,
+                      head_length = .25,
+                      fc = 'k',
+                      ec = 'k'
+                    )
+            #EAST pointer
+            if cell.getEast() == self.CS.eastBoundary:
+                dx = 0
+                dy = 0
+            elif truePointer:
+                dx = cell.getEast().cell.x + (.5 * cell.getEast().getWidth()) - (cell.cell.x + cell.getWidth()) + .5
+                dy = cell.getEast().cell.y + (.5 * cell.getEast().getHeight()) - (cell.cell.y + cell.getHeight()) + .5
+            else:
+                dx = .75
+                dy = 0
+            ax1.arrow((cell.cell.x + cell.getWidth() - .5),
+                      (cell.cell.y + cell.getHeight()- .5),
+                      dx,
+                      dy,
+                      head_width = .25,
+                      head_length = .25,
+                      fc = 'k',
+                      ec = 'k'
+                    )
+            #SOUTH pointer
+            if cell.getSouth() == self.CS.southBoundary:
+                dx = 0
+                dy = 0
+            elif truePointer:
+                dx = cell.getSouth().cell.x + (.5 * cell.getSouth().getWidth()) - (cell.cell.x + .5)
+                dy = cell.getSouth().cell.y + (.5 * cell.getSouth().getHeight()) - (cell.cell.y + .5)
+            else:
+                dx =  0
+                dy = -.5
+            ax1.arrow((cell.cell.x + .5),
+                      (cell.cell.y + .5),
+                      dx,
+                      dy,
+                      head_width = .25,
+                      head_length = .25,
+                      fc = 'k',
+                      ec = 'k'
+                    )
+            #WEST pointer
+            if cell.getWest() == self.CS.westBoundary:
+                dx = 0
+                dy = 0
+            elif truePointer:
+                dx = cell.getWest().cell.x + (.5 * cell.getWest().getWidth()) - (cell.cell.x + .5)
+                dy = cell.getWest().cell.y + (.5 * cell.getWest().getHeight()) - (cell.cell.y + .5)
+            else:
+                dx =  -.5
+                dy = 0
+            ax1.arrow((cell.cell.x + .5),
+                      (cell.cell.y + .5),
+                      dx,
+                      dy,
+                      head_width = .25,
+                      head_length = .25,
+                      fc = 'k',
+                      ec = 'k'
+                    )
+        p = PatchCollection(self.drawZeroDimsVertices())
+        ax1.add_collection(p)
+
+        #handle relative spacing from orientation to orientation-n (X0-Xn, Y0-Yn). Remove if refactoring to
+        #automatically handle orientation
+        self.setAxisLabels(plt)
+
+        for arr in self.findGraphEdges():
+            ax1.arrow(arr[0], arr[1], arr[2], arr[3], head_width = .30, head_length = .3, color = 'red')
+
+        plt.xlim(0, self.CS.eastBoundary.cell.x)
+        plt.ylim(0, self.CS.northBoundary.cell.y)
+        fig1.show()
+        pylab.pause(11000)  # figure out how to do this better
---
+++ powercad/corner_stitch/constraintGraph.py
@@ -0,0 +1,149 @@
+from sets import Set
+import numpy as np
+
+class constraintGraph:
+    """
+    the graph representation of constraints pertaining to cells and variables, informed by several different 
+    sources
+    """
+
+    def __init__(self, vertices, edges):
+        """
+        create from a given set of edges and vertices
+        vertexMatrix shows the connectivity between the vertices, it is a 2d list n vertices long
+        edges is just a set of edges
+        zeroDimensionList is where, independent of orientation, the list index corresponds to the graph's orientation
+        zero dimension representation. For example, in a very simple horizontally oriented 30 x30 plane,
+        where the only cell has a lower left corner of 5,15 and an upper right corner of 10, 25, zeroDimensionList will 
+        be as follows: [0]:0, [1]:5, [2]:10, [3]:30. Notice that it is arranged in increasing order, and we are only 
+        concerned with the x values, since this is horizontally oriented.
+        """
+        self.vertexMatrix = int[len(vertices)][len(vertices)]
+        self.edges = edges
+        self.zeroDimensionList = []
+
+    def __init__(self):
+        """
+        Default constructor
+        """
+        self.vertexMatrix = None
+        self.edges = []
+        self.zeroDimensionList = []
+
+    def getNeighbors(self, vertex):
+        """
+        return the edge types and neighbors of the vertex passed in
+        vertexMatrix shows the connectivity between the vertices, it is a 2d list n vertices long
+        edges is just a set of edges
+        zeroDimensionList is where, independent of orientation, the list index corresponds to the graph's orientation
+        zero dimension representation. For example, in a very simple horizontally oriented 30 x30 plane,
+        where the only cell has a lower left corner of 5,15 and an upper right corner of 10, 25, zeroDimensionList will 
+        be as follows: [0]:0, [1]:5, [2]:10, [3]:30. Notice that it is arranged in increasing order, and we are only 
+        concerned with the x values, since this is horizontally oriented.
+
+        """
+        return
+
+    def getEdgeType(self, v1, v2):
+        """
+        return the edge type from v1 to v2. If negative, then the edge type is from v2 to v1
+        """
+        return
+
+    def graphFromLayer(self, cornerStitch):
+        """
+        given a cornerStitch and an orientation = 'v' or 'h', construct a constraint graph detailing the dependencies of
+        one dimension point to another
+        """
+        self.dimListFromLayer(cornerStitch)
+        self.matrixFromDimList()
+        self.setEdgesFromLayer(cornerStitch)
+
+    def matrixFromDimList(self):
+        """
+        initializes a N x N matrix of 0's as self's matrix object
+        """
+        n = len(self.zeroDimensionList)
+        self.vertexMatrix = np.zeros((n, n), np.int8)
+
+    def printVM(self):
+        for i in self.vertexMatrix:
+            print i
+        return
+
+    def printZDL(self):
+        index = 0
+        for i in self.zeroDimensionList:
+            print index, ": ", i
+            index += 1
+
+    def setEdgesFromLayer(self, cornerStitch):
+        """
+        given a cornerStitch and orientation, set the connectivity matrix of this constraint graph
+        """
+        if cornerStitch.orientation == 'v':
+            for rect in cornerStitch.stitchList:
+                origin = self.zeroDimensionList.index(rect.cell.y)
+                dest = self.zeroDimensionList.index(rect.getNorth().cell.y)
+                self.vertexMatrix[origin][dest] = rect.getHeight()
+        elif cornerStitch.orientation == 'h':
+            for rect in cornerStitch.stitchList:
+                origin = self.zeroDimensionList.index(rect.cell.x)
+                dest = self.zeroDimensionList.index(rect.getEast().cell.x)
+                print "origin = ", origin, "dest = ", dest, "val = ", rect.getWidth()
+                self.vertexMatrix[origin][dest] = rect.getWidth()
+
+    def dimListFromLayer(self, cornerStitch):
+        """
+        generate the zeroDimensionList from a cornerStitch         
+        """
+        pointSet = Set() #this is a set of zero dimensional line coordinates, (e.g. x0, x1, x2, etc.)
+
+        if cornerStitch.orientation == 'v': #if orientation is vertical, add all unique y values for cells
+            for rect in cornerStitch.stitchList:
+                pointSet.add(rect.cell.y)
+            pointSet.add(cornerStitch.northBoundary.cell.y) # this won't be included in the normal list, so we do it here
+        elif cornerStitch.orientation == 'h': #same for horizontal orientation
+            for rect in cornerStitch.stitchList:
+                pointSet.add(rect.cell.x)
+            pointSet.add(cornerStitch.eastBoundary.cell.x)
+
+        asdf = list(pointSet)
+
+        self.zeroDimensionList =  asdf#setting the list of orientation values to an ordered list
+
+    def reduce(self):
+        """
+        This should call subroutines for edge and vertex reduction, to pare the constraint graph down to its 
+        minimum form
+        """
+        return
+
+    def edgeReduce(self):
+        """Eliminate redundant edges"""
+        return
+
+    def vertexReduce(self):
+        """Eliminate redundant vertices"""
+        return
+
+    def drawGraph(self):
+        G = nx.DiGraph()
+
+        it = np.nditer(self.vertexMatrix, flags=['multi_index'])
+        while not it.finished:
+            if it[0] != 0:
+                G.add_edges_from([it.multi_index], weight = it[0])
+                print it[0]
+            it.iternext()
+
+        edge_labels = dict([((u, v,), d['weight'])
+                            for u, v, d in G.edges(data=True)])
+
+        edge_colors = ['black' for edge in G.edges()]
+
+        pos = nx.shell_layout(G)
+        nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+        nx.draw_networkx_labels(G, pos)
+        nx.draw(G, pos, node_color='white', node_size=300, edge_color=edge_colors, edge_cmap=plt.cm.Reds)
+        pylab.show()
