7a4a93b3309d60a123ddceb2b81c725fd3092ae1
ialrazi <ialrazi@email.uark.edu>
2020-03-16 14:43:30 -0500

    commit before merge

--- powercad/cmd_run/cmd.py
+++ powercad/cmd_run/cmd.py
@@ -960,6 +960,8 @@ if __name__ == "__main__":
             cmd.cmd_handler_flow(arguments= args)
         elif sel==3:
             args = ['python','cmd.py','-m','D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Journal_Result_collection/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro.txt','-settings',"D:/Demo/New_Flow_w_Hierarchy/Journal_Case/settings.info"]
+            #D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Journal_Result_collection/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro.txt
+            #D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Testing_Journal_case_w_Py_3/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro.txt
             cmd.cmd_handler_flow(arguments= args)
 
     else:
