bd6d593b22d5837e5ae61d9dff1134ef7c2a50ae
ialrazi <ialrazi@email.uark.edu>
2020-04-08 19:35:26 -0500

    commit before switch.

---
+++ powercad/cmd_run/cmd_batch.py
@@ -0,0 +1,48 @@
+import sys, os
+
+# Set relative location
+cur_path =sys.path[0] # get current path (meaning this file location)
+cur_path = cur_path[0:-16] #exclude "power/cmd_run"
+sys.path.append(cur_path)
+from powercad.cmd_run.cmd import Cmd_Handler
+import os,glob
+import fileinput
+
+if __name__ == "__main__":
+    # print("----------------------PowerSynth Version 1.4: Command line version------------------")
+    
+    path = 'D:\Demo\\New_Flow_w_Hierarchy\Imam_journal\Cmd_flow_case\Imam_journal\Batch'
+    macro_files=[]
+    #macro_files =glob.glob("D:\Demo\\New_Flow_w_Hierarchy\Imam_journal\Cmd_flow_case\Imam_journal\Batch\macro_?.txt")
+    #macro_files = [f for f in os.listdir(path) if f.endswith('.txt')]
+    for f in os.listdir(path) :
+        if  f.endswith('.txt') and 'macro_' in f:
+            file=os.path.join(path,f)
+            for line in fileinput.input(file, inplace=True):
+                # for line in file:
+                line = line.replace('Plot_Solution: 1', 'Plot_Solution: 0')
+                sys.stdout.write(line)
+            macro_files.append(file) 
+    print(macro_files)
+    for file in macro_files:
+        print (file)
+        
+        debug = True
+        if debug: # you can mannualy add the argument in the list as shown here
+            
+            args = ['python','cmd.py','-m','D:/Demo/New_Flow_w_Hierarchy/Imam_journal/Cmd_flow_case/Imam_journal/half_bridge_pm_macro.txt','-settings',"D:/Demo/New_Flow_w_Hierarchy/Journal_Case/settings.info"]
+            #D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Journal_Result_collection/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro.txt
+            #D:\Demo\New_Flow_w_Hierarchy/Imam_journal/Cmd_flow_case/Imam_journal/half_bridge_pm_macro.txt
+            #D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Testing_Journal_case_w_Py_3/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro.txt
+            
+            #D:/Demo/New_Flow_w_Hierarchy/Journal_Case/Journal_Result_collection/Cmd_flow_case/Half_Bridge_Layout/half_bridge_pm_macro_data_collection_final.txt
+            
+            #macro_files = [f for f in os.listdir(path) if f.endswith('.txt')]
+            
+            args[3]=file
+            cmd = Cmd_Handler(debug=False)
+
+            cmd.cmd_handler_flow(arguments= args)
+
+        else:
+            cmd.cmd_handler_flow(arguments=sys.argv) # Default
